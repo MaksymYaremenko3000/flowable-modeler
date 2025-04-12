df355be509cf05a85ffb7912f3527ca8cf880119
Maksym Yaremenko <M.Yaremenko@mascode.com>
2025-04-12 23:28:32 +0300

    dsa

--- vue.config.js
+++ vue.config.js
@@ -32,11 +32,47 @@ if (NODE_ENV === 'lib') {
 }
 
 const mainFileStr = `
+
 import Vue from 'vue'
 import App from './Demo.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en'
+import xcrudConfig from 'xcrud/package/common/config'
+
+xcrudConfig.set({
+  select: {
+    placeholder: 'Select',
+    noDataText: 'No data',
+    noMatchText: 'No match',
+    loadingText: 'Loading...'
+  },
+  xtable: {
+    table: {
+      emptyText: 'No Data',
+      sumText: 'Total',
+    },
+    operate: {
+      column: {
+        label: 'Actions'
+      }
+    }
+  },
+  xform: {
+    form: {
+      tabs: {
+        table: {
+          addConfig: {
+            text: 'Add',
+          },
+          operate: {
+            label: 'Actions',
+          }
+        }
+      }
+    }
+  }
+})
 
 Vue.config.productionTip = false
 
@@ -45,6 +81,7 @@ Vue.use(ElementUI, { locale })
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
 `
 
 fs.writeFileSync('./src/main.js', mainFileStr)
